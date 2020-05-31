78a88785bace6cc88e2a28ef00a0c9ab803ad8c5
gruzzly-bear <facebawx@gmail.com>
2020-05-30 23:06:29 -0400

    Add SAYMUHNAME

---
+++ PersonsIThink/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsIThink
+{
+    class Employee : Person
+    {
+        int Id = 44578;
+    }
+}
---
+++ PersonsIThink/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PersonsIThink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee placeHolder = new Employee();
+
+           
+
+            placeHolder.FirstName = "Sample";
+            placeHolder.LastName = "Student";
+            placeHolder.SayName();
+        }
+    }
+}
