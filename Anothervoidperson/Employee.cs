61bc5d10f471b70839f405681b930907715f069c
gruzzly-bear <facebawx@gmail.com>
2020-05-30 23:46:01 -0400

    void add

---
+++ Anothervoidperson/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anothervoidperson
+{
+    public class Employee : Person
+    {
+        int Id = 44578;
+        public override void SayName()
+        {
+            Console.WriteLine("Name:" + firstName + " " + lastName);
+        }
+            
+}
+}
---
+++ Anothervoidperson/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Anothervoidperson
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee placeHolder = new Employee();
+
+
+
+            placeHolder.firstName = "Sample";
+            placeHolder.lastName = "Student";
+            placeHolder.SayName();
+        }
+    }
+}
