a9b6efcb6d04d269f5bf506b4c807f4184d6608d
gruzzly-bear <facebawx@gmail.com>
2020-05-30 22:39:14 -0400

    add deevide

---
+++ ConsoleApp1/Deevide.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Deevide
+    {
+        public void Boop(int inputOne)
+        {
+            int bop = inputOne / 2;
+            Console.WriteLine(bop);
+
+
+        }
+        static double Boop(double intOne)
+        {
+            double flop = intOne + .15;
+            return (double)flop;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Deevide blip = new Deevide();
+            Console.WriteLine("Enter a number!");
+            int inputOne = Convert.ToInt32(Console.ReadLine());
+            blip.Boop(inputOne);
+            Console.ReadLine();
+            Console.WriteLine("Enter a decimel!");
+            double intOne = Convert.ToDouble(Console.ReadLine());
+            blip.Boop((int)intOne);
+            Console.ReadLine();
+        }
+
+    }
+}
