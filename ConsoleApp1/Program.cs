4466ec26e3c3b40665474e83fa28dac8f529e982
gruzzly-bear <facebawx@gmail.com>
2020-05-31 17:50:23 -0400

    up

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,13 +8,13 @@ namespace ConsoleApp1
         {
             
             Deevide blip = new Deevide();
-            Console.WriteLine("Enter a number!");
+            Console.WriteLine("Enter a number! We will divide it by 2");
             int inputOne = Convert.ToInt32(Console.ReadLine());
             blip.Boop(inputOne);
             Console.ReadLine();
             Console.WriteLine("Enter another number!");
             int intOne2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter another number! if it is less than the last entered number!");
+            Console.WriteLine("Enter another number! We will see if it is less than the last entered number!");
             int intTwo = Convert.ToInt32(Console.ReadLine());
             bool done = false;
             bool flooop = blip.Boop(intOne2, intTwo, out done);
