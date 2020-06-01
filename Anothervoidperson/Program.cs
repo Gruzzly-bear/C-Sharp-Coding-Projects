7df0c95b67532c34fc67271ef780f3cad62c853f
gruzzly-bear <facebawx@gmail.com>
2020-06-01 18:15:18 -0400

    update

--- Anothervoidperson/Program.cs
+++ Anothervoidperson/Program.cs
@@ -12,12 +12,18 @@ namespace Anothervoidperson
         {
             Employee<string> strang = new Employee<string>();
             Employee<int> ant = new Employee<int>();
-            ant.ADD(4);
-            strang.Add("Dog");
-            
-            
-            
-            
+            ant.Things.Add(4);
+            ant.Things.Add(7);
+            ant.Things.Add(15);
+            ant.Things.Add(25);
+            strang.Things.Add("Bob");
+            strang.Things.Add("Jim");
+            strang.Things.Add("Steve");
+            strang.Things.Add("Phil");
+
+            Console.WriteLine(strang);
+            Console.WriteLine(ant);
+
             Console.ReadLine();
            
             // IQuittable quits = new Employee();
