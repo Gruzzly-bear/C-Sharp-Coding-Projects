014477f2070e2e4ef0d5ddf48a0ab6d21b84eb5f
gruzzly-bear <facebawx@gmail.com>
2020-05-31 23:27:25 -0400

    Update OVerload.cs

--- PersonsIThink/OVerload.cs
+++ PersonsIThink/OVerload.cs
@@ -8,16 +8,27 @@ namespace PersonsIThink
     {
         public static Employee operator ==(Employee Employee1, Employee Employee2)
         {
+            
             if (Employee1.Id == Employee2.Id)
             {
-                Console.WriteLine("They match!");
+
+                Console.WriteLine(Employee1 + "and" + Employee2 + "They match!");
+            }
+            else
+            {
+                Console.WriteLine(Employee1 + "and" + Employee2 + "DO NOT MATCH");
             }
         }
+            
         public static Employee operator !=(Employee Employee1, Employee Employee2)
         {
             if (Employee1.Id != Employee2.Id) 
             {
                 Console.WriteLine("They DO NOT match!");
+            }
+            else
+            {
+                Console.WriteLine("They match!");
             }
         }
     }
