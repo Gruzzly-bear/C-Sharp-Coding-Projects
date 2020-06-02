79a70b81c22bfbc8870fc764f672858b6318b54d
gruzzly-bear <facebawx@gmail.com>
2020-06-02 18:26:15 -0400

    updoot void

--- Anothervoidperson/Employee.cs
+++ Anothervoidperson/Employee.cs
@@ -6,8 +6,10 @@ namespace Anothervoidperson
 {
     public class Employee<T> : Person, IQuittable
     {
-        int Id = 44578;
-        public List<T> Things { get; set; } = new List<T>();
+        //int Id = 44578;
+        public List<T> Fname { get; set; } = new List<T>();
+        public List<T> Lname { get; set; } = new List<T>();
+        public List<T> Id { get; set; } = new List<T>();
         public void Quit()
         {
             throw new NotImplementedException();
