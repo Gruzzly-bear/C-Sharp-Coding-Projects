d39d82edcf5edf4ab467e7b7b73738759959d4f3
gruzzly-bear <facebawx@gmail.com>
2020-06-02 17:22:42 -0400

    add struct

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number amount1 = new Number();
+            amount1.Amount = 45.7;
+            Console.WriteLine(amount1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Struct/Struct/structOne.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Struct
+{
+    public struct Number
+    {
+        public double Amount { get; set; }
+    }
+}
