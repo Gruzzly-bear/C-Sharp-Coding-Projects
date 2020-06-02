baeb3759baea9280620db78ade81297338a9fbba
gruzzly-bear <facebawx@gmail.com>
2020-06-01 21:56:09 -0400

    Final void updoot

--- Anothervoidperson/Employee.cs
+++ Anothervoidperson/Employee.cs
@@ -7,7 +7,7 @@ namespace Anothervoidperson
     public class Employee<T> : Person, IQuittable
     {
         int Id = 44578;
-        public List<T> Things { get; set; }
+        public List<T> Things { get; set; } = new List<T>();
         public void Quit()
         {
             throw new NotImplementedException();
--- Anothervoidperson/Program.cs
+++ Anothervoidperson/Program.cs
@@ -13,6 +13,8 @@ namespace Anothervoidperson
             
             Employee<string> strang = new Employee<string>();
             Employee<int> ant = new Employee<int>();
+            
+
             ant.Things.Add(4);
             ant.Things.Add(7);
             ant.Things.Add(15);
@@ -21,10 +23,16 @@ namespace Anothervoidperson
             strang.Things.Add("Jim");
             strang.Things.Add("Steve");
             strang.Things.Add("Phil");
-
-            Console.WriteLine(strang);
-            Console.WriteLine(ant);
-
+            Console.WriteLine("Here are the Ints in the newly added list.");
+            foreach (object o in ant.Things)
+            {
+                Console.WriteLine(o);
+            }
+            Console.WriteLine("Here are the Strings in the newly added list.");
+            foreach (object o in strang.Things)
+            {
+                Console.WriteLine(o);
+            }
             Console.ReadLine();
            
             // IQuittable quits = new Employee();
