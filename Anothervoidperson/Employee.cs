38323fa865aeb0afe7b3601ac5d12cb1b1feced2
gruzzly-bear <facebawx@gmail.com>
2020-06-04 18:32:34 -0400

    Add edited for new course

--- Anothervoidperson/Employee.cs
+++ Anothervoidperson/Employee.cs
@@ -4,12 +4,13 @@ using System.Text;
 
 namespace Anothervoidperson
 {
-    public class Employee<T> : Person, IQuittable
+    //public class Employee<T> : Person, IQuittable
+    public class Employee : Person, IQuittable
     {
         //int Id = 44578;
-        public List<T> Fname { get; set; } = new List<T>();
-        public List<T> Lname { get; set; } = new List<T>();
-        public List<T> Id { get; set; } = new List<T>();
+       // public List<T> Fname { get; set; } = new List<T>();
+       //public List<T> Lname { get; set; } = new List<T>();
+        //public List<T> Id { get; set; } = new List<T>();
         public void Quit()
         {
             throw new NotImplementedException();
--- Anothervoidperson/Program.cs
+++ Anothervoidperson/Program.cs
@@ -10,39 +10,39 @@ namespace Anothervoidperson
     {
         static void Main(string[] args)
         {
-            
-            Employee<string> Fname = new Employee<string>();
-            Employee<int> Id = new Employee<int>();
-            Employee<string> Lname = new Employee<string>();
+            Employee placeHolder = new Employee();
+           // Employee<string> Fname = new Employee<string>();
+           // Employee<int> Id = new Employee<int>();
+            //Employee<string> Lname = new Employee<string>();
 
 
-            Id.Id.Add(4);
-            Id.Id.Add(7);
-            Id.Id.Add(15);
-            Id.Id.Add(25);
-            Fname.Fname.Add("Joe");
-            Fname.Fname.Add("Jim");
-            Fname.Fname.Add("Steve");
-            Fname.Fname.Add("Joe");
-            Lname.Lname.Add("Parks");
-            Lname.Lname.Add("Roberts");
-            Lname.Lname.Add("Stevens");
-            Lname.Lname.Add("Smith");
+            //Id.Id.Add(4);
+            //Id.Id.Add(7);
+            //Id.Id.Add(15);
+            //Id.Id.Add(25);
+            //Fname.Fname.Add("Joe");
+            //Fname.Fname.Add("Jim");
+            //Fname.Fname.Add("Steve");
+            //Fname.Fname.Add("Joe");
+            //Lname.Lname.Add("Parks");
+            //Lname.Lname.Add("Roberts");
+            //Lname.Lname.Add("Stevens");
+            //Lname.Lname.Add("Smith");
           //  Console.WriteLine("Here are the Ints in the newly added list.");
-            string myString = "Joe";
-            string element = "";
-            for (int i = 0; i < Fname.Fname.Count; i++)
-            {
-                if (Fname.Fname[i].Contains(myString))
-                    element = Fname.Fname[i];
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("LAMBDA TIME!!!?!!!!!");
-            Console.ReadLine();
-            var newList = Fname.Fname.Where(x => x.Contains(myString)).ToList();
-            Console.WriteLine(newList);
-            var newList2 = Id.Id.Where(xx => xx > 5).ToList();
-            Console.WriteLine(newList2);
+            //string myString = "Joe";
+            //string element = "";
+            //for (int i = 0; i < Fname.Fname.Count; i++)
+            //{
+            //    if (Fname.Fname[i].Contains(myString))
+            //        element = Fname.Fname[i];
+            //    Console.WriteLine(element);
+            //}
+            //Console.WriteLine("LAMBDA TIME!!!?!!!!!");
+            //Console.ReadLine();
+            //var newList = Fname.Fname.Where(x => x.Contains(myString)).ToList();
+            //Console.WriteLine(newList);
+            //var newList2 = Id.Id.Where(xx => xx > 5).ToList();
+            //Console.WriteLine(newList2);
             //  Console.WriteLine("Here are the Strings in the newly added list.");
             //  foreach (object o in Fname.Things)
             //  {
@@ -53,9 +53,9 @@ namespace Anothervoidperson
             // IQuittable quits = new Employee();
             // quits.Quit();
 
-            //placeHolder.firstName = "Sample";
-            //placeHolder.lastName = "Student";
-            //placeHolder.SayName();
+            placeHolder.firstName = "Sample";
+            placeHolder.lastName = "Student";
+            placeHolder.SayName();
 
 
         }
