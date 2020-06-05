c0f4901d492038e73701d7ffb09c0e0840c526f5
gruzzly-bear <facebawx@gmail.com>
2020-06-04 21:23:22 -0400

    add 7

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace ConsoleApp7
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Assign Assign = new Assign();
+
+			Console.WriteLine("Please enter a number!");
+			int inputOne = Convert.ToInt32(Console.ReadLine());
+			int methodResult = Assign.ThisThing(inputOne);
+			Console.WriteLine("We are going to multiply it by 10!");
+			Console.WriteLine(inputOne + " multiplied by ten equals " + methodResult);
+			Console.WriteLine("Please enter a decimal number!");
+			float inputTwo = Convert.ToSingle(Console.ReadLine());
+			int methodTwo = Assign.ThisThing(inputTwo);
+			Console.WriteLine("We are going to Math it hard");
+			Console.WriteLine(inputTwo + " Plus 40.67 = " + methodTwo);
+			Console.Write("Please input a number ");
+			string inputThree = (Console.ReadLine());
+			int methodThree = Assign.ThisThing(inputThree);
+			Console.Write(" We will add two! ");
+			Console.WriteLine(inputThree + " Plus Two = " + methodThree);
+
+
+
+
+		}
+	}
+}
+
---
+++ ConsoleApp7/assign.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp7
+{
+	public class Assign
+	{
+
+		public int ThisThing(int inputOne)
+		{
+
+			int doMath = inputOne * 10;
+			return doMath;
+
+
+		}
+
+		public int ThisThing(float inputTwo)
+		{
+			float ohNoes = (float)(inputTwo + 40.67);
+			return (int)ohNoes;
+
+		}
+		public int ThisThing(string inputThree)
+		{
+			
+			int number;
+			
+
+			bool isParsable = Int32.TryParse(inputThree, out number);
+
+			if (isParsable)
+
+				return number + 2;
+			else
+				Console.WriteLine(inputThree + "Is not a number");
+			return number;
+
+
+
+		}
+
+
+	}
+}
