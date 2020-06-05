ae1765d0c8e673019b9b1f890bc54ac9cccc03a8
gruzzly-bear <facebawx@gmail.com>
2020-06-04 20:03:58 -0400

    uipdoot

---
+++ Enummiesprt2/Enummiesprt2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Enummiesprt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("What day is it today?");
+                string today = Console.ReadLine().ToLower(); 
+                Day inputDay = (Day)Enum.Parse(typeof(Day), today);
+                if (Enum.TryParse(today, out Day dayResult))
+                {
+                    Console.WriteLine("Today's day is " + dayResult);
+                }
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
