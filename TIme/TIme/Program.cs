c403d0f970dd2d190d7a4ec50e150609ddeed74d
gruzzly-bear <facebawx@gmail.com>
2020-06-06 15:56:59 -0400

    add time

---
+++ TIme/TIme/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data;
+
+namespace TIme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = DateTime.Now;
+            Console.WriteLine("The time and Date is " + currentTime.ToString());
+            Console.WriteLine("Please enter a number!");
+            int inputOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("It will be " + DateTime.Now.AddHours(inputOne) +", " + inputOne + " hours from now!" );
+            Console.ReadLine();
+        }
+
+    }
+}
+
