81d0aab240a4c2aedf4447f2ad9b6ac7a9f93c2e
gruzzly-bear <facebawx@gmail.com>
2020-06-06 23:11:20 -0400

    add Age

---
+++ Age Exception/Age Exception/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Age_Exception
+{
+    class Program
+    {
+
+    public class AgeException : Exception 
+        {                                     
+            public AgeException()
+                : base() { }
+            public AgeException(string message)
+            : base(message) { }
+        }
+        static void Main()
+        {
+
+
+            int inputAge;
+            try
+            {
+                Console.WriteLine("How old are you?");
+                inputAge = int.Parse(Console.ReadLine());
+                if (inputAge <= 0)
+                {
+                    throw new AgeException();
+                }
+                var today = DateTime.Now;
+                var ageYear = today.Year - inputAge;
+                Console.WriteLine("You were born in " + ageYear);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please input numbers only.");
+            }
+            catch (AgeException)
+            {
+                Console.WriteLine("Enter a number greater than 0!");
+            }
+            catch (Exception) 
+            {
+                Console.WriteLine("An Error occured. Please contact your System Admin.");
+                Console.ReadLine();
+                return;
+            }
+            Console.ReadLine();
+        }
+    }
+}
