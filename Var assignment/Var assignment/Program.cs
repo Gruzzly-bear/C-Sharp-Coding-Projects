e8433f205690926179d4ada971d902f67cb864f1
gruzzly-bear <facebawx@gmail.com>
2020-06-06 21:58:54 -0400

    add var assignment

---
+++ Var assignment/Var assignment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Var_assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Matt";
+            var age = 29;
+            Console.WriteLine(name + " is " + age + " years old!");
+            Console.ReadLine();
+        }
+    }
+}
