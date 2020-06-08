a6f8c2d678d46cdda676e5f63cfcc335240592a3
gruzzly-bear <facebawx@gmail.com>
2020-06-07 20:33:59 -0400

    add new

---
+++ ConsoleApp2/ConsoleApp2/Constructors.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    public class Fname
+    {
+        public Fname(string inputName) : this (inputName, 100)
+        {
+
+        }
+        public Fname(string inputName, int inputHealth)
+        {
+            
+        }
+        public Fname() { }
+        public string inputName { get; set; }
+        public int inputHealth { get; set; }
+
+
+
+
+
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using Constructors;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            Fname fname = new Fname();
+            Fname newFname = new Fname("World Boss");
+            Console.WriteLine("Please enter your name!");
+            var firstName = Console.ReadLine();
+            Console.WriteLine("Please enter your starting health!");
+            var inputHealth = Convert.ToInt32(Console.ReadLine());
+            fname.inputHealth = inputHealth;
+            Console.WriteLine("Your name is " + firstName + " and your starting health is " + fname.inputHealth );
+            Console.ReadLine();
+            Console.WriteLine(" You will battle " + newFname.inputName + " whose health is at " + newFname.inputHealth);
+
+
+
+
+        }
+        }
+    }
