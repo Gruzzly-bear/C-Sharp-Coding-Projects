bd9952b13956b1f77a061ce4798b6a9ad5566d57
gruzzly-bear <facebawx@gmail.com>
2020-06-25 18:49:29 -0400

    update to add all

--- foreachlist/foreachlist/Program.cs
+++ foreachlist/foreachlist/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace foreachlist
 {
@@ -7,6 +8,54 @@ namespace foreachlist
     {
         static void Main(string[] args)
         {
+            string[] animalList = { " Dog", " Cat", " Fish", " Rabbit", " Bear" };
+            Console.WriteLine("Please enter a color");
+            var colorInput = Console.ReadLine();
+            foreach (var l in animalList)
+            {
+                Console.WriteLine(colorInput + l);
+            }
+            Console.ReadLine();
+
+
+            Console.WriteLine("Here is our infinite loop");
+            int f = 0;
+            while (f < 10) // was while (true)
+            {
+                Console.WriteLine(f);
+                f++;
+            }
+            Console.WriteLine("We fixed it, so we can now continue");
+            Console.ReadLine();
+            Console.WriteLine("Here is a for loop using <");
+            Console.ReadLine();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Value of o: {0}", i);
+            }
+            Console.WriteLine("Here is a for loop using <=");
+            Console.ReadLine();
+            for (int o = 0; o <= 10; o++)
+            {
+                Console.WriteLine("Value of o: {0}", o);
+            }
+            string[] shapes = { "Square", "Circle", "Triangle", "Rectangle", "Dog" };
+            Console.WriteLine("Please search either : Circle, Square, Triangle, Dog, or Rectangle.");
+            var inputOne = Console.ReadLine();
+            for (int z = 0; z < 6; z++) 
+            { 
+                if (shapes.Contains(inputOne))
+                {
+                    var index = Array.FindIndex(shapes, row => row.Contains(inputOne));
+                    Console.WriteLine("The index is " + index);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Was not on the list!");
+                
+                }
+            }
             string[] nameList = { "Bob", "Tom", "Ryan", "Phil", "Ryan" };
             var myList = new List<string>();
             var copies = new List<string>();
